e5f93cf17351ab7cd9bab8ea99f05aec44c64773
UnlimitedBytes <petersonchristian896@gmail.com>
2020-05-28 15:01:53 +0200

    Update vuetify dark color scheme

--- nuxt.config.js
+++ nuxt.config.js
@@ -134,10 +134,10 @@ export default {
                     primary: colors.blue.darken2,
                     accent: colors.grey.darken3,
                     secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
+                    info: colors.blue.darken3,
+                    warning: colors.amber.darken3,
+                    error: colors.deepOrange.darken3,
+                    success: colors.green.darken3
                 }
             }
         }
